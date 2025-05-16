69fa97a01364322193fd277694a3f31e8ae98ff4
amirmohamadshabani <amirmoohamadshabani@gmail.com>
2025-05-16 22:33:22 +0330

    Update server.js
    for render.com

--- server.js
+++ server.js
@@ -3,37 +3,25 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-// سرو کردن فایل‌های استاتیک مثل HTML, CSS, JS
 app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
-  console.log("user new connect:", socket.id);
+    console.log("User connected:", socket.id);
 
-  // وقتی کاربر دستور پاک کردن داد
-  socket.on("clearCanvas", () => {
-    // ارسال دستور پاک کردن برای همه کاربران
-    io.emit("clearCanvas");
-  });
+    socket.on("clearCanvas", () => {
+        io.emit("clearCanvas");
+    });
 
-  // وقتی کاربر شروع به کشیدن خط می‌کند
-  socket.on("startLine", (data) => {
-    // ارسال به سایر کاربران
-    socket.broadcast.emit("receiveStartLine", data);
-  });
+    socket.on("startLine", (data) => {
+        socket.broadcast.emit("receiveStartLine", data);
+    });
 
-  // وقتی کاربر در حال نقاشی است
-  socket.on("drawing", (data) => {
-    // ارسال به سایر کاربران
-    socket.broadcast.emit("receiveDrawing", data);
-  });
-
-  // وقتی کاربر قطع شد
-  socket.on("disconnect", () => {
-    console.log("user disconnect:", socket.id);
-  });
+    socket.on("drawing", (data) => {
+        socket.broadcast.emit("receiveDrawing", data);
+    });
 });
 
-const PORT = process.env.PORT || 3000;// عدد رو میتونیم تغییر بدیم
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`سرور در پورت ${PORT} در حال اجراست`);
+    console.log(`Server is running on port ${PORT}`);
 });
